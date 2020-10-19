347bc5f49b5d757f3fa4ab512536b2c30df73ae0
Varun Bhatia <tsalitzion@gmail.com>
2020-10-19 10:17:04 +0900

    Create create_markdown.sh

---
+++ create_markdown.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+
+cd usr/src/app
+
+mkdir clone_url_repo
+
+cd clone_url_repo
+
+git clone $1
+
+cd $2
+
+git diff --name-status origin/$4 origin/$3 > /usr/src/app/markdown.txt
+
+cd /usr/src/app 
+
+rm -r clone_url_repo
